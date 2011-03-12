dd27c801b11ba810ac6239b07bc7d10fd1dbcb16
Werner Lemberg <wl@gnu.org>
2011-03-12 16:47:59 +0100

    Simplify error handling.

--- src/ttfautohint.c
+++ src/ttfautohint.c
@@ -26,15 +26,15 @@ TA_Error
 TTF_autohint(FILE *in,
              FILE *out)
 {
-  FT_Library lib;
-  FT_Face face;
-  FT_Long num_faces;
-  FT_ULong num_tables;
+  FT_Library lib = NULL;
+  FT_Face face = NULL;
+  FT_Long num_faces = 0;
+  FT_ULong num_tables = 0;
 
   FT_Byte* in_buf;
   size_t in_len;
 
-  SFNT_Table* SFNT_Tables;
+  SFNT_Table* SFNT_Tables = NULL;
   FT_ULong glyf_idx;
 
   FT_Error error;
@@ -68,30 +68,30 @@ TTF_autohint(FILE *in,
 
   error = FT_New_Memory_Face(lib, in_buf, in_len, -1, &face);
   if (error)
-    goto Err1;
+    goto Err;
   num_faces = face->num_faces;
   FT_Done_Face(face);
 
   error = FT_New_Memory_Face(lib, in_buf, in_len, 0, &face);
   if (error)
-    goto Err1;
+    goto Err;
 
   /* check that font is TTF */
   if (!FT_IS_SFNT(face))
   {
     error = FT_Err_Invalid_Argument;
-    goto Err2;
+    goto Err;
   }
 
   error = FT_Sfnt_Table_Info(face, 0, NULL, &num_tables);
   if (error)
-    goto Err2;
+    goto Err;
 
   SFNT_Tables = (SFNT_Table*)calloc(1, num_tables * sizeof (SFNT_Table));
   if (!SFNT_Tables)
   {
     error = FT_Err_Out_Of_Memory;
-    goto Err2;
+    goto Err;
   }
 
   /* collect SFNT table data */
@@ -103,7 +103,7 @@ TTF_autohint(FILE *in,
 
     error = FT_Sfnt_Table_Info(face, i, &tag, &len);
     if (error && error != FT_Err_Table_Missing)
-      goto Err3;
+      goto Err;
 
     if (!error)
     {
@@ -125,7 +125,7 @@ TTF_autohint(FILE *in,
   if (glyf_idx == num_tables)
   {
     error = FT_Err_Invalid_Argument;
-    goto Err3;
+    goto Err;
   }
 
 
@@ -143,12 +143,12 @@ TTF_autohint(FILE *in,
         if (!st->buf)
         {
           error = FT_Err_Out_Of_Memory;
-          goto Err4;
+          goto Err;
         }
 
         error = FT_Load_Sfnt_Table(face, st->tag, 0, st->buf, &st->len);
         if (error)
-          goto Err4;
+          goto Err;
 
         st++;
       }
@@ -172,16 +172,14 @@ TTF_autohint(FILE *in,
 
   error = TA_Err_Ok;
 
-Err4:
+Err:
+  /* in case of error it is expected that the unallocated pointers */
+  /* are NULL (and counters are zero) */
   for (i = 0; i < num_tables; i++)
     free(SFNT_Tables[i].buf);
-Err3:
   free(SFNT_Tables);
-Err2:
   FT_Done_Face(face);
-Err1:
   FT_Done_FreeType(lib);
-Err:
   free(in_buf);
 
   return error;
