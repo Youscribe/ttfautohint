e376913a8fbd6a72d15d5b7cbeaeb75de67638f2
Werner Lemberg <wl@gnu.org>
2011-03-13 08:06:50 +0100

    Split TTF_autohint into smaller functions.
    Use new FONT structure to collect variables.

--- src/ttfautohint.c
+++ src/ttfautohint.c
@@ -11,6 +11,7 @@
 
 #include <ft2build.h>
 #include FT_FREETYPE_H
+#include FT_TRUETYPE_TABLES_H
 #include FT_TRUETYPE_TAGS_H
 
 #include "ttfautohint.h"
@@ -18,7 +19,6 @@
 
 /* this structure represents both the data contained in the SFNT */
 /* table records and the actual SFNT table data */
-
 typedef struct SFNT_Table_ {
   FT_ULong tag;
   FT_ULong len;
@@ -26,231 +26,298 @@ typedef struct SFNT_Table_ {
 } SFNT_Table;
 
 /* this structure is used to model a font within a TTC */
-
 typedef struct SFNT_ {
   FT_Face face;
   SFNT_Table* table_infos;
   FT_ULong num_table_infos;
 } SFNT;
 
-
-TA_Error
-TTF_autohint(FILE *in,
-             FILE *out)
-{
-  FT_Library lib = NULL;
+/* our font object */
+typedef struct FONT_ {
+  FT_Library lib;
 
   FT_Byte* in_buf;
   size_t in_len;
 
   SFNT* sfnts;
-  FT_Long num_sfnts = 0;
+  FT_Long num_sfnts;
 
-  SFNT_Table* tables = NULL;
-  FT_ULong num_tables = 0;
-
-  FT_Error error;
-
-  FT_Long i;
-  FT_ULong j;
+  SFNT_Table* tables;
+  FT_ULong num_tables;
+} FONT;
 
 
-  /*************************/
-  /*                       */
-  /* load font into memory */
-  /*                       */
-  /*************************/
-
+static FT_Error
+TA_font_load_into_memory(FILE *in,
+                         FONT *font)
+{
   fseek(in, 0, SEEK_END);
-  in_len = ftell(in);
+  font->in_len = ftell(in);
   fseek(in, 0, SEEK_SET);
 
   /* a TTF can never be that small */
-  if (in_len < 100)
+  if (font->in_len < 100)
     return FT_Err_Invalid_Argument;
 
-  in_buf = (FT_Byte*)malloc(in_len);
-  if (!in_buf)
+  font->in_buf = (FT_Byte*)malloc(font->in_len);
+  if (!font->in_buf)
     return FT_Err_Out_Of_Memory;
 
-  if (fread(in_buf, 1, in_len, in) != in_len)
-  {
-    error = FT_Err_Invalid_Stream_Read;
-    goto Err;
-  }
+  if (fread(font->in_buf, 1, font->in_len, in) != font->in_len)
+    return FT_Err_Invalid_Stream_Read;
 
-  error = FT_Init_FreeType(&lib);
+  return TA_Err_Ok;
+}
+
+
+static FT_Error
+TA_font_init(FONT *font)
+{
+  FT_Error error;
+  FT_Face f;
+
+
+  error = FT_Init_FreeType(&font->lib);
   if (error)
-    goto Err;
+    return error;
 
-  /* get number of faces */
+  /* get number of faces (i.e. subfonts) */
+  error = FT_New_Memory_Face(font->lib, font->in_buf, font->in_len, -1, &f);
+  if (error)
+    return error;
+  font->num_sfnts = f->num_faces;
+  FT_Done_Face(f);
+
+  /* it is a TTC if we have more than a single subfont */
+  font->sfnts = (SFNT*)calloc(1, font->num_sfnts * sizeof (SFNT));
+  if (!font->sfnts)
+    return FT_Err_Out_Of_Memory;
+
+  return TA_Err_Ok;
+}
+
+
+static FT_Error
+TA_font_collect_table_info(SFNT *sfnt)
+{
+  FT_Error error;
+  FT_ULong glyf_idx;
+  FT_ULong i;
+
+
+  /* check that font is TTF */
+  if (!FT_IS_SFNT(sfnt->face))
+    return FT_Err_Invalid_Argument;
+
+  error = FT_Sfnt_Table_Info(sfnt->face, 0, NULL, &sfnt->num_table_infos);
+  if (error)
+    return error;
+
+  sfnt->table_infos =
+    (SFNT_Table*)calloc(1, sfnt->num_table_infos * sizeof (SFNT_Table));
+  if (!sfnt->table_infos)
+    return FT_Err_Out_Of_Memory;
+
+  /* collect SFNT table information and search for `glyf' table */
+  glyf_idx = sfnt->num_table_infos;
+  for (i = 0; i < sfnt->num_table_infos; i++)
   {
-    FT_Face f;
+    FT_ULong tag, len;
 
 
-    error = FT_New_Memory_Face(lib, in_buf, in_len, -1, &f);
-    if (error)
-      goto Err;
-    num_sfnts = f->num_faces;
-    FT_Done_Face(f);
-  }
+    error = FT_Sfnt_Table_Info(sfnt->face, i, &tag, &len);
+    if (error && error != FT_Err_Table_Missing)
+      return error;
 
-  /* it is a TTC if we have more than a single face */
-  sfnts = (SFNT*)calloc(1, num_sfnts * sizeof (SFNT));
-  if (!sfnts)
-  {
-    error = FT_Err_Out_Of_Memory;
-    goto Err;
-  }
-
-  for (i = 0; i < num_sfnts; i++)
-  {
-    FT_ULong glyf_idx;
-
-    SFNT *sfnt = &sfnts[i];
-
-
-    error = FT_New_Memory_Face(lib, in_buf, in_len, i, &sfnt->face);
-    if (error)
-      goto Err;
-
-    /* check that font is TTF */
-    if (!FT_IS_SFNT(sfnt->face))
+    if (!error)
     {
-      error = FT_Err_Invalid_Argument;
-      goto Err;
-    }
+      if (tag == TTAG_glyf)
+        glyf_idx = i;
 
-    error = FT_Sfnt_Table_Info(sfnt->face, 0, NULL, &sfnt->num_table_infos);
-    if (error)
-      goto Err;
-
-    sfnt->table_infos =
-      (SFNT_Table*)calloc(1, sfnt->num_table_infos * sizeof (SFNT_Table));
-    if (!sfnt->table_infos)
-    {
-      error = FT_Err_Out_Of_Memory;
-      goto Err;
-    }
-
-    /* collect SFNT table information and search for `glyf' table */
-    glyf_idx = sfnt->num_table_infos;
-    for (j = 0; j < sfnt->num_table_infos; j++)
-    {
-      FT_ULong tag, len;
-
-
-      error = FT_Sfnt_Table_Info(sfnt->face, j, &tag, &len);
-      if (error && error != FT_Err_Table_Missing)
-        goto Err;
-
-      if (!error)
+      /* ignore tables which we are going to create by ourselves */
+      if (!(tag == TTAG_fpgm
+            || tag == TTAG_prep
+            || tag == TTAG_cvt))
       {
-        if (tag == TTAG_glyf)
-          glyf_idx = j;
-
-        /* ignore tables which we are going to create by ourselves */
-        if (!(tag == TTAG_fpgm
-              || tag == TTAG_prep
-              || tag == TTAG_cvt))
-        {
-          sfnt->table_infos[j].tag = tag;
-          sfnt->table_infos[j].len = len;
-        }
+        sfnt->table_infos[i].tag = tag;
+        sfnt->table_infos[i].len = len;
       }
     }
+  }
 
-    /* no (non-empty) `glyf' table; this can't be a TTF with outlines */
-    if (glyf_idx == sfnt->num_table_infos)
+  /* no (non-empty) `glyf' table; this can't be a TTF with outlines */
+  if (glyf_idx == sfnt->num_table_infos)
+    return FT_Err_Invalid_Argument;
+
+  return TA_Err_Ok;
+}
+
+
+static FT_Error
+TA_font_split_SFNT_tables(SFNT *sfnt,
+                          FONT *font)
+{
+  FT_Error error;
+  FT_ULong i;
+
+
+  for (i = 0; i < sfnt->num_table_infos; i++)
+  {
+    SFNT_Table *table_info = &sfnt->table_infos[i];
+
+
+    /* we ignore empty tables */
+    if (table_info->len)
     {
-      error = FT_Err_Invalid_Argument;
-      goto Err;
-    }
+      FT_Byte* buf;
+      FT_ULong j;
 
 
-    /*******************************/
-    /*                             */
-    /* split font into SFNT tables */
-    /*                             */
-    /*******************************/
+      buf = (FT_Byte*)malloc(table_info->len);
+      if (!buf)
+        return FT_Err_Out_Of_Memory;
 
-    for (j = 0; j < sfnt->num_table_infos; j++)
-    {
-      SFNT_Table *table_info = &sfnt->table_infos[j];
+      /* load table */
+      error = FT_Load_Sfnt_Table(sfnt->face, table_info->tag, 0,
+                                 buf, &table_info->len);
+      if (error)
+        goto Err;
+
+      /* check whether we already have this table */
+      for (j = 0; j < font->num_tables; j++)
+      {
+        SFNT_Table *table = &font->tables[j];
 
 
-      /* we ignore empty tables */
-      if (table_info->len)
+        if (table->tag == table_info->tag
+            && table->len == table_info->len
+            && !memcmp(table->buf, buf, table->len))
+          break;
+      }
+
+      /* add element to table array if it's missing or not the same */
+      if (j == font->num_tables)
       {
         SFNT_Table* tables_new;
         SFNT_Table* table_last;
-        SFNT_Table* table;
-        FT_Byte* buf;
-
-        FT_Long k;
 
 
-        buf = (FT_Byte*)malloc(table_info->len);
-        if (!buf)
+        font->num_tables++;
+        tables_new =
+          (SFNT_Table*)realloc(font->tables,
+                               font->num_tables * sizeof (SFNT_Table));
+        if (!tables_new)
         {
           error = FT_Err_Out_Of_Memory;
           goto Err;
         }
-
-        /* load table */
-        error = FT_Load_Sfnt_Table(sfnt->face, table_info->tag, 0,
-                                   buf, &table_info->len);
-        if (error)
-          goto Err1;
-
-        /* check whether we already have this table */
-        for (k = 0; k < num_tables; k++)
-        {
-          table = &tables[k];
-
-          if (table->tag == table_info->tag
-              && table->len == table_info->len
-              && !memcmp(table->buf, buf, table->len))
-            break;
-        }
-
-        /* add one element to table array if it's missing or not the same */
-        if (k == num_tables)
-        {
-          num_tables++;
-          tables_new =
-            (SFNT_Table*)realloc(tables,
-                                 num_tables * sizeof (SFNT_Table));
-          if (!tables_new)
-          {
-            error = FT_Err_Out_Of_Memory;
-            goto Err1;
-          }
-          else
-            tables = tables_new;
-
-          table_last = &tables[num_tables - 1];
-
-          table_last->tag = table_info->tag;
-          table_last->len = table_info->len;
-          table_last->buf = buf;
-
-          /* link buffer pointer */
-          table_info->buf = table_last->buf;
-        }
         else
-        {
-          free(buf);
-          table_info->buf = tables[k].buf;
-        }
-        continue;
+          font->tables = tables_new;
 
-      Err1:
-        free(buf);
-        goto Err;
+        table_last = &font->tables[font->num_tables - 1];
+
+        table_last->tag = table_info->tag;
+        table_last->len = table_info->len;
+        table_last->buf = buf;
+
+        /* link buffer pointer */
+        table_info->buf = table_last->buf;
       }
+      else
+      {
+        free(buf);
+        table_info->buf = font->tables[j].buf;
+      }
+      continue;
+
+    Err:
+      free(buf);
+      return error;
     }
   }
+
+  return TA_Err_Ok;
+}
+
+
+static void
+font_unload(FONT *font)
+{
+  if (!font)
+    return;
+
+  /* in case of error it is expected that unallocated pointers */
+  /* are NULL (and counters are zero) */
+  if (font->tables)
+  {
+    FT_ULong i;
+
+
+    for (i = 0; i < font->num_tables; i++)
+      free(font->tables[i].buf);
+    free(font->tables);
+  }
+
+  if (font->sfnts)
+  {
+    FT_Long i;
+
+
+    for (i = 0; i < font->num_sfnts; i++)
+    {
+      FT_Done_Face(font->sfnts[i].face);
+      free(font->sfnts[i].table_infos);
+    }
+    free(font->sfnts);
+  }
+
+  FT_Done_FreeType(font->lib);
+  free(font->in_buf);
+  free(font);
+}
+
+
+TA_Error
+TTF_autohint(FILE *in,
+             FILE *out)
+{
+  FONT* font;
+  FT_Error error;
+  FT_Long i;
+
+
+  font = (FONT*)calloc(1, sizeof (FONT));
+  if (!font)
+    return FT_Err_Out_Of_Memory;
+
+  error = TA_font_load_into_memory(in, font);
+  if (error)
+    goto Err;
+
+  error = TA_font_init(font);
+  if (error)
+    goto Err;
+
+  /* loop over subfonts */
+  for (i = 0; i < font->num_sfnts; i++)
+  {
+    SFNT *sfnt = &font->sfnts[i];
+
+
+    error = FT_New_Memory_Face(font->lib, font->in_buf, font->in_len,
+                               i, &sfnt->face);
+    if (error)
+      goto Err;
+
+    error = TA_font_collect_table_info(sfnt);
+    if (error)
+      goto Err;
+
+    error = TA_font_split_SFNT_tables(sfnt, font);
+    if (error)
+      goto Err;
+  }
+
 
   /* compute global hints */
   /* construct `fpgm' table */
@@ -270,25 +337,7 @@ TTF_autohint(FILE *in,
   error = TA_Err_Ok;
 
 Err:
-  /* in case of error it is expected that unallocated pointers */
-  /* are NULL (and counters are zero) */
-  if (tables)
-  {
-    for (j = 0; j < num_tables; j++)
-      free(tables[j].buf);
-    free(tables);
-  }
-  if (sfnts)
-  {
-    for (i = 0; i < num_sfnts; i++)
-    {
-      FT_Done_Face(sfnts[i].face);
-      free(sfnts[i].table_infos);
-    }
-    free(sfnts);
-  }
-  FT_Done_FreeType(lib);
-  free(in_buf);
+  font_unload(font);
 
   return error;
 }
