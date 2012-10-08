51c4c6a69d588956fdca6635563d3b6e1ddf5d49
Werner Lemberg <wl@gnu.org>
2012-10-08 15:26:44 +0200

    [doc] Updated.

--- lib/ttfautohint.h
+++ lib/ttfautohint.h
@@ -86,8 +86,7 @@ typedef int TA_Error;
  * (this value can't be larger than 65535).
  *
  * *curr_sfnt* gives the current subfont within a TrueType Collection (TTC),
- * and *num_sfnts* the total number of subfonts.  Currently, the ttfautohint
- * library only hints glyphs from the `glyf` table used in subfont\ 0.
+ * and *num_sfnts* the total number of subfonts.
  *
  * If the return value is non-zero, `TTF_autohint` aborts with
  * `TA_Err_Canceled`.  Use this for a 'Cancel' button or similar features in
